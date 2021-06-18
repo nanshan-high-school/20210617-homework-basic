3c397ef6c4768921513bd9b0cb3d0792056ccaf5
howard <howard60211@gmail.com>
2021-06-18 19:27:39 +0800

    Add annotation

--- 755299/UseIg.cpp
+++ 755299/UseIg.cpp
@@ -8,11 +8,14 @@
 #define BOLDBLUE "\033[1m\033[34m"
 #define RESET "\033[0m"
 
+//Emoji constructor
 Emoji::Emoji(std::string faceType):shape(std::move(faceType)), count(0){}
 
+//Emoji overload operator function ++ & --
 void Emoji::operator ++ () { this->count++; }
 void Emoji::operator -- () { this->count--; }
 
+//Enter user felling
 void Poster::rate(){
     int rater=0;
     std::cout << "input (1:like 2:angry 3:happy 4:cry 5:break) :";
@@ -56,14 +59,17 @@ void Poster::rate(){
     }
 }
 
+//Poster constructor
 Poster::Poster():like(u8"\U00002665"), angry(u8"\U0001F4A2"),
                  happy(u8"\U0001F603"), cry(u8"\U0001F622"), emojied(0){}
 
+//Emoji overload operator function <<(out)
 std::ostream & operator <<(std::ostream & out, const Emoji& a){
     out << a.shape << " " << a.count << "   ";
     return out;
 }
 
+//Poster overload operator function >>(in) & <<(out)
 std::istream & operator >> (std::istream & in, Poster & a){
     std::cout << "at (stop with input '*'):";
     std::string input;
@@ -93,17 +99,20 @@ std::ostream & operator <<(std::ostream & outer, const Poster& a){
     return outer;
 }
 
+//IgServer function login
 void IgServer::login(){
     std::cout << "Input login account (you don't need password) :";
     std::cin >> this->loginUser;
 }
 
+//IgServer helper function batchInput
 void IgServer::batchInput() {
     Poster batchPost;
     std::cin >> batchPost;
     this->PostStack::push(batchPost);
 }
 
+//IgServer overload operator function <<(out) & >>(in)
 std::basic_ostream<char> & operator << (std::basic_ostream<char> & out, IgServer & a){
     for(auto it = a.begin(); it != a.end(); it++){
         out << a.loginUser;
@@ -126,6 +135,7 @@ std::basic_istream<char> & operator >> (std::basic_istream<char> & in, IgServer 
     return in;
 }
 
+//IgServer function reaction
 void IgServer::reaction(){
     int post_num;
     while(true){
