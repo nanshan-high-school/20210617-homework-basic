37f4c2cd289213bf13829500ba9ddc1ab94f194c
Jifong <jason950321@gmail.com>
2021-07-24 22:52:49 +0800

    新增 按讚 留言

--- 755227/Instagram.cpp
+++ 755227/Instagram.cpp
@@ -10,10 +10,16 @@ int main() {
         string photo;
         string article;
         string time;
+        string thumb;
+        int thumbNum;
+        int leaveMessageNum;
     };
 
     stack<ig> allPost;
     ig newPost = {};
+    int thumbNum;
+    string thumb;
+    stack<string> thumbPeople;
 
     do {
         cout << "輸入使用者ID(輸入「0」以結束): ";
@@ -31,6 +37,28 @@ int main() {
         long nowTime = time(0) + 28800;
         newPost.time = ctime(&nowTime);
 
+        do {
+            cout << "輸入按讚之使用者ID(以換行隔開)(輸入「0」以結束): ";
+            cin >> thumb;
+
+            if (thumb != "0") {
+                thumbNum++;
+                thumbPeople.push(thumb);
+            }
+            
+        } while (thumb != "0");
+        
+        newPost.thumbNum = thumbNum;
+        thumbNum = 0;
+
+        if (! thumbPeople.empty()) {
+            newPost.thumb = thumbPeople.top();
+        }
+
+        cout << "輸入留言數量: ";
+        cin >> newPost.leaveMessageNum;
+
+        cout << endl;
         allPost.push(newPost);
     } while (true);
 
@@ -39,15 +67,28 @@ int main() {
     while(! allPost.empty()) {
         ig showPost = allPost.top();
         
-        cout << showPost.user << endl;
+        cout << "使用者 " << showPost.user << " 發布了貼文" << endl;
+
         cout << showPost.photo << endl;
+
         cout << showPost.article << endl;
-        cout << showPost.time;
+
+        cout << "發布於 " << showPost.time << endl;
+
+        if (showPost.thumbNum == 0) {
+            cout << "無人按讚" << endl;
+        } else if (showPost.thumbNum == 1) {
+            cout << showPost.thumb << "按讚" << endl;
+        } else if (showPost.thumbNum >= 2) {
+            cout << showPost.thumb << "和其他" << showPost.thumbNum - 1 << "人按讚" << endl;
+        }
+
+        cout << showPost.leaveMessageNum << "則留言" << endl;
         
         allPost.pop();
 
         if (! allPost.empty()) {
-            cout << "-------------" << endl;
+            cout << "-------------" << endl << endl;
         }
     }
 }
